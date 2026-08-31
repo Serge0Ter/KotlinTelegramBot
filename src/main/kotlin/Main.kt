964c8ef254bf7a19149eea2bfe786a08f20c024f
Tereschin_SV <tereshin_ser@>
2026-08-31 10:58:16 +0300

    KTB-02: Создать первичную версию словаря

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,8 +5,11 @@ import java.io.File
 fun main() {
 
     val words: File = File("words.txt")
-    for (word in words.readLines()) {
-        println(word)
+    if (words.exists()) {
+        for (word in words.readLines()) {
+            println(word)
+        }
     }
+
 
 }

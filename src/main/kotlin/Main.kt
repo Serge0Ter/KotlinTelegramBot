2f27f866fcb7c7d9d1c272d9112f4a034b2b4119
Tereschin_SV <tereshin_ser@>
2026-09-02 09:42:21 +0300

    KTB-04: Спарсить файл словаря и распечатать содержимое

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,13 +2,16 @@ package org.example
 
 import java.io.File
 
+private val dictionary = mutableListOf<Word>()
+
 fun main() {
 
     val words: File = File("words.txt")
 
     try {
         for (word in words.readLines()) {
-            println(word)
+            val newWord = word.split("|")
+            dictionary.add(Word(newWord[0], newWord[1], newWord[3].toInt()))
         }
     } catch (e: Exception) {
         println(e.message)

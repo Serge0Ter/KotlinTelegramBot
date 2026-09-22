44fd7af4b926f91bfbbae2c68a89b7d5189c49c9
Tereschin_SV <tereshin_ser@>
2026-09-22 14:30:03 +0300

    KTB-12: Получить токен телеграм-бота и выполнить базовые HTTP запросы

--- src/main/kotlin/telegram.kt
+++ src/main/kotlin/telegram.kt
@@ -9,13 +9,24 @@ private const val BASE_URL = "https://api.telegram.org/bot"
 fun main(args: Array<String>) {
 
     val botToken = args[0]
-    println(getUpdates(botToken))
+    var updateId = 0
+
+    while (true) {
+        Thread.sleep(2000)
+        val updates = getUpdates(botToken, updateId)
+        println(updates)
+        val startUpdateId = updates.lastIndexOf("update_id")
+        val endUpdateId = updates.lastIndexOf(",\n\"message\"")
+        if (startUpdateId == -1 || endUpdateId == -1) continue
+        val updateIdString = updates.substring(startUpdateId + 11, endUpdateId)
+        updateId = updateIdString.toInt() + 1
+    }
     println(getMe(botToken))
 
 }
 
-fun getUpdates(botToken: String): String {
-    val urlGetUpdates = "$BASE_URL$botToken/getUpdates"
+fun getUpdates(botToken: String, updateId: Int): String {
+    val urlGetUpdates = "$BASE_URL$botToken/getUpdates?offset=$updateId"
     val client: HttpClient = HttpClient.newBuilder().build()
     val request: HttpRequest = HttpRequest.newBuilder().uri(URI.create(urlGetUpdates)).build()
     val response: HttpResponse<String> = client.send(request, HttpResponse.BodyHandlers.ofString())

d2f9015f2cbd638e52719e4b695f59d0b6589940
Tereschin_SV <tereshin_ser@>
2026-09-22 16:18:17 +0300

    KTB-13: Отправить запрос с параметрами и распарсить ответ

--- src/main/kotlin/telegram.kt
+++ src/main/kotlin/telegram.kt
@@ -6,22 +6,23 @@ import java.net.http.HttpRequest
 import java.net.http.HttpResponse
 
 private const val BASE_URL = "https://api.telegram.org/bot"
+private const val UPDATE_ID_KEY = "\"update_id\":"
 fun main(args: Array<String>) {
 
     val botToken = args[0]
     var updateId = 0
-
+    println(getMe(botToken))
     while (true) {
         Thread.sleep(2000)
         val updates = getUpdates(botToken, updateId)
         println(updates)
-        val startUpdateId = updates.lastIndexOf("update_id")
-        val endUpdateId = updates.lastIndexOf(",\n\"message\"")
-        if (startUpdateId == -1 || endUpdateId == -1) continue
-        val updateIdString = updates.substring(startUpdateId + 11, endUpdateId)
+        val startUpdateId = updates.indexOf(UPDATE_ID_KEY)
+        if (startUpdateId == -1) continue
+        val endUpdateId = updates.indexOf(",", startUpdateId)
+        if (endUpdateId == -1) continue
+        val updateIdString = updates.substring(startUpdateId + UPDATE_ID_KEY.length, endUpdateId)
         updateId = updateIdString.toInt() + 1
     }
-    println(getMe(botToken))
 
 }
 
